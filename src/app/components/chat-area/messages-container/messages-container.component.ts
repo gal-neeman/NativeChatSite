e0971a9cc16cebe207358cd3152cf01ab837c06c
gal-neeman <galneeman4@gmail.com>
2025-06-01 19:32:43 +0300

    fixed scroll position after chat change and sending a message

--- src/app/components/chat-area/messages-container/messages-container.component.ts
+++ src/app/components/chat-area/messages-container/messages-container.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, input } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Message } from '../../../models/message.model';
 import { MessageComponent } from "../message/message.component";
 import { ChatAgent } from '../../../models/chatAgent.model';
@@ -12,7 +12,10 @@ import { User } from '../../../models/user.model';
   styleUrl: './messages-container.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesContainerComponent {
+export class MessagesContainerComponent implements AfterViewChecked {
+  @ViewChild('messagesContainer')
+  messagesContainer: ElementRef;
+
   @Input()
   messages: Message[];
 
@@ -22,18 +25,23 @@ export class MessagesContainerComponent {
   @Input()
   user: User;
 
-   public getSenderAgent(message: Message) : ChatAgent {
-      if (this.bot.id == message.senderId &&
-          this.user.id == message.receiverId
-      ) {
-        return {id: this.bot.id, name: this.bot.name};
-      }
-      else if (this.user.id == message.senderId &&
-              this.bot.id == message.receiverId
-      ) {
-        return {id: this.user.id, name: this.user.username};
-      }
-  
-      return null;
+  // Scroll to bottom when changing view
+  ngAfterViewChecked(): void {
+    this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+  }
+
+  public getSenderAgent(message: Message) : ChatAgent {
+    if (this.bot.id == message.senderId &&
+        this.user.id == message.receiverId
+    ) {
+      return {id: this.bot.id, name: this.bot.name};
     }
+    else if (this.user.id == message.senderId &&
+            this.bot.id == message.receiverId
+    ) {
+      return {id: this.user.id, name: this.user.username};
+    }
+
+    return null;
+  }
 }
